c7883f0daa997b0650eee8b93177805cc05feb37
Samson Bitrus <bitruss@gmail.com>
2018-05-24 01:31:53 -0400

    Update proxyBuilder.py

--- proxyBuilder.py
+++ proxyBuilder.py
@@ -2,4 +2,6 @@ from selenium import webdriver
 import time
 
 # We use firefox driver on a link that will give us list of elite proxys
+driver = webdriver.Firefox()
 driver.get("http://www.gatherproxy.com/proxylist/anonymity/?t=Elite")
+SuperClass = driver.find_element_by_class_name('proxy-lists')
