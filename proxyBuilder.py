fa1f903f0bc866ee1924ad37ebea8b31b0c708e9
Samson Bitrus <bitruss@gmail.com>
2018-05-24 02:31:28 -0400

    Update proxyBuilder.py
    Checked the code to see if there was any change to the site

--- proxyBuilder.py
+++ proxyBuilder.py
@@ -1,7 +1,28 @@
 from selenium import webdriver
-import time
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from random import randint
 
-# We use firefox driver on a link that will give us list of elite proxys
-driver = webdriver.Firefox()
+# Run Firefox as headless because we don't want a browser window to pop-up
+options = Options()
+options.add_argument("--headless")
+
+driver = webdriver.Firefox(firefox_options=options)
 driver.get("http://www.gatherproxy.com/proxylist/anonymity/?t=Elite")
-SuperClass = driver.find_element_by_class_name('proxy-lists')
+
+_randNum = randint(3, 27)
+
+# From the page-source structure,  the 'tr' tag in the 'tbody' have two tr's that we want to ignore
+# and the page displays 25 Elite IP addresses and ports
+# From above we generated a random number between 3 and 27 to pick an IP randomly while ignoring the 
+# first two 'tr' tags
+_ip = driver.find_element_by_xpath("//div[@class='proxy-list']/table/tbody/tr[%d]/td[2]"%_randNum).text
+_port = driver.find_element_by_xpath("//div[@class='proxy-list']/table/tbody/tr[%d]/td[3]"%_randNum).text
+
+def _getIP():
+	global _ip
+	return _ip
+def _getPort():
+	global _port
+	return int(_port)
+driver.close()
